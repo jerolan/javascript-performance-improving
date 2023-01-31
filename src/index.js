27c9b842d0e76df39aae90c45e7611dfb95a2ee2
Jerome Olvera <jerome.olvera@gmail.com>
2023-01-31 05:16:14 +0000

    add base pokemon list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+const pokemonCardTemplate = document.querySelector("#pokemon-card");
+const fragment = document.createDocumentFragment();
+
 function getPokemons() {
-  return fetch("https://pokeapi.co/api/v2/pokemon");
+  return fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((result) => result.json())
+    .then(({ results }) => results);
 }
+
+function render(content) {
+  content.forEach(({ name }, index) => {
+    const number = index + 1;
+    const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`;
+    const pokemonCard = pokemonCardTemplate.content.cloneNode(true);
+    pokemonCard.querySelector(".pokemon-card__name").textContent = name;
+    pokemonCard.querySelector(
+      ".pokemon-card__number"
+    ).textContent = `#${number}`;
+    pokemonCard.querySelector(".pokemon-card__image").alt = name;
+    pokemonCard.querySelector(".pokemon-card__image").src = url;
+    pokemonCard.querySelector(
+      ".pokemon-card__link"
+    ).href = `/pokemon?id=${number}`;
+    fragment.appendChild(pokemonCard);
+  });
+  pokemonCardTemplate.parentNode.appendChild(fragment);
+}
+
+getPokemons().then((content) => render(content));
