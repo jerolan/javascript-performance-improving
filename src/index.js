451925f85ffbd4579d7dfdd3c9132ad99b470619
Jerome Olvera <jerome.olvera@gmail.com>
2023-02-06 20:24:47 +0000

    extend examples

--- src/index.js
+++ src/index.js
@@ -7,40 +7,40 @@ function getPokemons() {
     .then(({ results }) => results);
 }
 
-function render(content) {
+function render(content, optimze = false) {
   const head = document.querySelector("head");
 
   content.forEach(({ name }, index) => {
+    // arrange variables
     const number = index + 1;
     const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`;
     const pokemonCard = pokemonCardTemplate.content.cloneNode(true);
 
+    // override template values
     pokemonCard.querySelector(".pokemon-card__name").textContent = name;
-    pokemonCard.querySelector(
-      ".pokemon-card__number"
-    ).textContent = `#${number}`;
+    pokemonCard.querySelector(".pokemon-card__number").textContent = `#${number}`;
     pokemonCard.querySelector(".pokemon-card__image").alt = name;
-    pokemonCard
-      .querySelector(".pokemon-card__image")
-      .setAttribute("data-src", img);
+    pokemonCard.querySelector(".pokemon-card__image").setAttribute("src", img);
+    pokemonCard.querySelector(".pokemon-card__link").href = `/pokemon?id=${number}`;
 
-    pokemonCard.querySelector(
-      ".pokemon-card__link"
-    ).href = `/pokemon?id=${number}`;
+    // apply performance optimization
+    if (optimze) {
+      pokemonCard.querySelector(".pokemon-card__image").removeAttribute("src");
+      pokemonCard.querySelector(".pokemon-card__image").setAttribute("data-src", img);
 
-    if (index >= 3 && index < 15) {
-      const link = document.createElement("link");
-      link.rel = "prefetch";
-      link.as = "image";
-      link.href = img;
-      head.appendChild(link);
+      if (index >= 3 && index < 15) {
+        const link = document.createElement("link");
+        link.rel = "prefetch";
+        link.as = "image";
+        link.href = img;
+        head.appendChild(link);
+      }
+
+      makeLazy(pokemonCard.querySelector(".pokemon-card__image"));
     }
 
-    makeLazy(pokemonCard.querySelector(".pokemon-card__image"));
     fragment.appendChild(pokemonCard);
   });
 
   pokemonCardTemplate.parentNode.appendChild(fragment);
 }
-
-getPokemons().then((content) => render(content));
---
+++ src/slow.js
@@ -0,0 +1,10 @@
+function slow() {
+  function fibo(n) {
+    if (n < 2) return 1;
+    else return fibo(n - 2) + fibo(n - 1);
+  }
+
+  console.log(fibo(43));
+}
+
+slow();
