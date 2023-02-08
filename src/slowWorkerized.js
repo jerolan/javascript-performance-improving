8788d1cde90aa92ad3f876d2883db5de682c7e40
Jerome Olvera <jerome.olvera@gmail.com>
2023-02-08 00:08:25 +0000

    fix variable destructuring

--- src/slowWorkerized.js
+++ src/slowWorkerized.js
@@ -1,8 +1,8 @@
-addEventListener("message", (event) => {
+addEventListener("message", ({data}) => {
   function fibo(n) {
     if (n < 2) return 1;
     else return fibo(n - 2) + fibo(n - 1);
   }
 
-  console.log(fibo(event));
+  console.log(fibo(data));
 });
